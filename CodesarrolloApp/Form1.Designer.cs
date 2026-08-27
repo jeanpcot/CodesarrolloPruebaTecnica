7381493c7f356aee16fed80b98841e1f7fba292e
Jean Cotera <jean.cotera@gmail.com>
2026-08-27 14:32:17 -0700

    Cambios en la presentación del formulario

--- CodesarrolloApp/Form1.Designer.cs
+++ CodesarrolloApp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             dgvAlumnos = new DataGridView();
             lbDisponibles = new ListBox();
             cmbSeleccionados = new ComboBox();
@@ -37,7 +38,11 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            btnOrdenarInverso = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvAlumnos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dgvAlumnos
@@ -46,7 +51,7 @@
             dgvAlumnos.AllowUserToDeleteRows = false;
             dgvAlumnos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvAlumnos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvAlumnos.Location = new Point(12, 59);
+            dgvAlumnos.Location = new Point(12, 171);
             dgvAlumnos.Name = "dgvAlumnos";
             dgvAlumnos.ReadOnly = true;
             dgvAlumnos.RowHeadersWidth = 51;
@@ -56,53 +61,54 @@
             // lbDisponibles
             // 
             lbDisponibles.FormattingEnabled = true;
-            lbDisponibles.Location = new Point(643, 81);
+            lbDisponibles.Location = new Point(13, 567);
             lbDisponibles.Name = "lbDisponibles";
-            lbDisponibles.Size = new Size(150, 104);
+            lbDisponibles.Size = new Size(175, 104);
             lbDisponibles.TabIndex = 1;
             lbDisponibles.MouseDoubleClick += lbDisponibles_MouseDoubleClick;
             // 
             // cmbSeleccionados
             // 
             cmbSeleccionados.FormattingEnabled = true;
-            cmbSeleccionados.Location = new Point(804, 81);
+            cmbSeleccionados.Location = new Point(194, 567);
             cmbSeleccionados.Name = "cmbSeleccionados";
-            cmbSeleccionados.Size = new Size(151, 28);
+            cmbSeleccionados.Size = new Size(175, 28);
             cmbSeleccionados.TabIndex = 2;
             cmbSeleccionados.SelectedIndexChanged += cmbSeleccionados_SelectedIndexChanged;
             // 
             // btnOrdenar
             // 
-            btnOrdenar.Location = new Point(12, 13);
+            btnOrdenar.Location = new Point(12, 123);
             btnOrdenar.Name = "btnOrdenar";
-            btnOrdenar.Size = new Size(218, 29);
+            btnOrdenar.Size = new Size(56, 29);
             btnOrdenar.TabIndex = 3;
-            btnOrdenar.Text = "Ordenar por orden alfabético";
+            btnOrdenar.Text = "A - Z";
             btnOrdenar.UseVisualStyleBackColor = true;
             btnOrdenar.Click += btnOrdenar_Click;
             // 
             // txtDescripcion
             // 
-            txtDescripcion.Location = new Point(643, 232);
+            txtDescripcion.Location = new Point(375, 567);
             txtDescripcion.Multiline = true;
             txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(150, 62);
+            txtDescripcion.Size = new Size(262, 104);
             txtDescripcion.TabIndex = 4;
             // 
             // btnAgregar
             // 
-            btnAgregar.Location = new Point(819, 371);
+            btnAgregar.BackColor = SystemColors.Control;
+            btnAgregar.Location = new Point(496, 120);
             btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(136, 29);
+            btnAgregar.Size = new Size(141, 35);
             btnAgregar.TabIndex = 5;
             btnAgregar.Text = "Agregar alumno";
-            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.UseVisualStyleBackColor = false;
             btnAgregar.Click += btnAgregar_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(643, 58);
+            label1.Location = new Point(12, 544);
             label1.Name = "label1";
             label1.Size = new Size(87, 20);
             label1.TabIndex = 6;
@@ -111,7 +117,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(804, 58);
+            label2.Location = new Point(194, 544);
             label2.Name = "label2";
             label2.Size = new Size(104, 20);
             label2.TabIndex = 7;
@@ -120,17 +126,49 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(643, 209);
+            label3.Location = new Point(375, 544);
             label3.Name = "label3";
             label3.Size = new Size(87, 20);
             label3.TabIndex = 8;
             label3.Text = "Descripción";
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Codesarrollo_logo;
+            pictureBox1.Location = new Point(183, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(285, 103);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Ordenar nombres:";
+            // 
+            // btnOrdenarInverso
+            // 
+            btnOrdenarInverso.Location = new Point(85, 123);
+            btnOrdenarInverso.Name = "btnOrdenarInverso";
+            btnOrdenarInverso.Size = new Size(56, 29);
+            btnOrdenarInverso.TabIndex = 12;
+            btnOrdenarInverso.Text = "Z - A";
+            btnOrdenarInverso.UseVisualStyleBackColor = true;
+            btnOrdenarInverso.Click += btnOrdenarInverso_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(967, 413);
+            ClientSize = new Size(650, 683);
+            Controls.Add(btnOrdenarInverso);
+            Controls.Add(label4);
+            Controls.Add(pictureBox1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -140,9 +178,11 @@
             Controls.Add(cmbSeleccionados);
             Controls.Add(lbDisponibles);
             Controls.Add(dgvAlumnos);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
-            Text = "Alumnos";
+            Text = "Lista de alumnos";
             ((System.ComponentModel.ISupportInitialize)dgvAlumnos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -158,5 +198,8 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Button btnOrdenarInverso;
     }
 }
--- CodesarrolloApp/Form2.Designer.cs
+++ CodesarrolloApp/Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             txtNombre = new TextBox();
             chkActivo = new CheckBox();
             txtDescripcion = new TextBox();
@@ -36,19 +37,22 @@
             label1 = new Label();
             label3 = new Label();
             label4 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(184, 51);
+            txtNombre.Font = new Font("Segoe UI", 14F);
+            txtNombre.Location = new Point(12, 59);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(196, 27);
+            txtNombre.Size = new Size(401, 39);
             txtNombre.TabIndex = 0;
             // 
             // chkActivo
             // 
             chkActivo.AutoSize = true;
-            chkActivo.Location = new Point(183, 179);
+            chkActivo.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkActivo.Location = new Point(125, 298);
             chkActivo.Name = "chkActivo";
             chkActivo.Size = new Size(18, 17);
             chkActivo.TabIndex = 2;
@@ -57,17 +61,19 @@
             // 
             // txtDescripcion
             // 
-            txtDescripcion.Location = new Point(184, 95);
+            txtDescripcion.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDescripcion.Location = new Point(12, 168);
             txtDescripcion.Multiline = true;
             txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(197, 64);
+            txtDescripcion.Size = new Size(401, 88);
             txtDescripcion.TabIndex = 1;
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(75, 249);
+            btnCancelar.Font = new Font("Segoe UI", 16F);
+            btnCancelar.Location = new Point(22, 401);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.Size = new Size(176, 49);
             btnCancelar.TabIndex = 4;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
@@ -75,46 +81,63 @@
             // 
             // btnConfirmar
             // 
-            btnConfirmar.Location = new Point(286, 249);
+            btnConfirmar.BackColor = SystemColors.Control;
+            btnConfirmar.Font = new Font("Segoe UI", 16F);
+            btnConfirmar.Location = new Point(226, 401);
             btnConfirmar.Name = "btnConfirmar";
-            btnConfirmar.Size = new Size(94, 29);
+            btnConfirmar.Size = new Size(176, 49);
             btnConfirmar.TabIndex = 3;
             btnConfirmar.Text = "Confirmar";
-            btnConfirmar.UseVisualStyleBackColor = true;
+            btnConfirmar.UseVisualStyleBackColor = false;
             btnConfirmar.Click += btnConfirmar_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(75, 54);
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 18);
             label1.Name = "label1";
-            label1.Size = new Size(70, 20);
+            label1.Size = new Size(131, 38);
             label1.TabIndex = 6;
             label1.Text = "Nombre*";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(75, 176);
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 287);
             label3.Name = "label3";
-            label3.Size = new Size(51, 20);
+            label3.Size = new Size(93, 38);
             label3.TabIndex = 8;
             label3.Text = "Activo";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(76, 116);
+            label4.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 127);
             label4.Name = "label4";
-            label4.Size = new Size(93, 20);
+            label4.Size = new Size(174, 38);
             label4.TabIndex = 9;
             label4.Text = "Descripción*";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 7.20000029F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(98, 344);
+            label2.Name = "label2";
+            label2.Size = new Size(228, 17);
+            label2.TabIndex = 10;
+            label2.Text = "Todos los valores con * son obligatorios";
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(449, 337);
+            ClientSize = new Size(425, 492);
+            Controls.Add(label2);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label1);
@@ -123,6 +146,7 @@
             Controls.Add(txtDescripcion);
             Controls.Add(chkActivo);
             Controls.Add(txtNombre);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form2";
             Text = "Agregar alumno";
             ResumeLayout(false);
