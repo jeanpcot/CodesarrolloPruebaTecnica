fd561d0a3e371f8b63faab3b647a81dc4e857106
Jean Cotera <jean.cotera@gmail.com>
2026-08-27 09:20:58 -0700

    Creacion de la clase Alumno

---
+++ CodesarrolloApp/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodesarrolloApp
+{
+    internal class Alumno
+    {
+        // Propiedades solicitadas en el punto 2
+        // Al ser publicas se pone con mayuscula la primera letra de cada palabra
+        public int Identificador { get; set; }
+        public string Nombre { get; set; }
+        public bool Activo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
